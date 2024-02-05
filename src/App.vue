71cf7b01fdc6a8a3502cdba6538e5b161ac64d76
qingyou2517 <2517789608@qq.com>
2024-02-06 01:08:58 +0800

    feat: 使用vscode开发uniapp项目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <image mode="aspectFill"></image>
+</template>
+
+<script setup lang="ts">
+import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
+onLaunch(() => {
+  console.log("App Launch");
+});
+onShow(() => {
+  console.log("App Show");
+});
+onHide(() => {
+  console.log("App Hide");
+});
+</script>
+<style></style>
